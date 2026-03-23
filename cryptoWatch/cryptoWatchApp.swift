9809e38927db88f9d25e52ab364266745bfec168
Dennis <den@gmail.com>
2026-03-22 19:52:41 -0700

    Initial Commit

---
+++ cryptoWatch/cryptoWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptoWatchApp.swift
+//  cryptoWatch
+//
+//  Created by Dennis Okafor on 22/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct cryptoWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
